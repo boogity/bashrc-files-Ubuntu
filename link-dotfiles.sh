5e0bcedd6deb9c2ed671b0d03c533a164389c9f6
boogity <harumph505@gmail.com>
2024-07-26 10:00:26 -0500

    Update symlink script to work if original symlinks are broken. TODO: Sort out and identify differences between .shells/ files: EXPORTS and DEFAULTS TODO: .shells is currently symlinked within the repo dir as well (e.g., ~/Repos/bashrc-files/.shells/.shells). But only when we already have a .shells symlink in homedir...

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -4,14 +4,23 @@ scriptDir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 #for object in $(basename $(find . -maxdepth 1 \( ! -name .git -and ! -name *.sh \))); do
 for object in $(find . -maxdepth 1 \( ! -name '.git' -and ! -name *.sh \) -exec basename {} \;) ; do
+	echo "===== FOUND DOTFILE: $object ====="
     if [[ "$object" =~ ^.$ ]]; then
-	:
+        # Don't symlink CWD ('.')
+        echo "Doing nothing for $object"
+    elif [[ "$object" =~ '.gitignore' ]]; then
+        # Don't symlink .gitignore into $HOME
+        echo "Doing nothing for $object"
     else
-        echo "Creating symlink for $HOME/$object"
-        if [[ -e "$HOME/$object" ]]; then
+        # Symlink everything else
+        echo "Creating symlink to $HOME/$object"
+
+        # Backup existing files first though
+        # Is it a file? Is it a dir? Is it a symlink (handles broken symlinks)
+        if [[ -e "$HOME/$object" || -d "$HOME/$object" || -L "$HOME/$object" ]]; then
             echo "$object already exists in $HOME, backing up to $HOME/$object.BAK_$(date +%Y-%m-%d)"
             mv -f "$HOME/$object" "$HOME/$object.BAK_$(date +%Y-%m-%d)"
-	fi
-        ln -s "$scriptDir/$object" ~/${object}
+        fi
+        ln -s "$scriptDir/$object" "$HOME/$object"
     fi
 done
